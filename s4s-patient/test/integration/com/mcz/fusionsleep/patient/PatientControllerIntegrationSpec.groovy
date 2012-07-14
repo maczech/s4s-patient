93397b2163408cf29f66584ea10635596f58f3bc
maczech <maczech@gamil.com>
2012-07-14 18:15:06 +0200

    Patient Controller test added

--- s4s-patient/test/integration/com/mcz/fusionsleep/patient/PatientControllerIntegrationSpec.groovy
+++ s4s-patient/test/integration/com/mcz/fusionsleep/patient/PatientControllerIntegrationSpec.groovy
@@ -13,6 +13,8 @@ class PatientControllerIntegrationSpec extends IntegrationSpec{
 	def setup(){
 		controller = new PatientController();
 		controller.compilanceService = compilanceService
+		
+		Patient.list()*.delete()
 	}
 	
 	def "should fill compilance data when list"() {
@@ -26,6 +28,29 @@ class PatientControllerIntegrationSpec extends IntegrationSpec{
 		
 		then:
 		assert responseMap.patientInstanceList.compilance != null;
+		
+		where:
+		firstName="Marcin"
+		lastName="Czech";
+		gender = Patient.Gender.MALE;
+		status = Patient.Status.INITIAL;
+		
+		
+	}
+	
+	def "should fill remove patient"() {
+		setup:
+		def location = new Location(code:"1",name:"Test location 1").save(failOnError:true)
+		def patient = new Patient(firstName:firstName,lastName:lastName,gender:gender,status: status,location:location)
+		.save(failOnError:true)
+		
+		when:
+		controller.params.id = patient.id;
+		controller.delete()
+		
+		then:"redirect to list view"
+		assert Patient.count == 0
+		
 		
 		where:
 		firstName="Marcin"
